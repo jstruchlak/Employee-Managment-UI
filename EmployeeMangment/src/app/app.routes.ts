de09bf85e9629447e00e1060e13bb79e6f81cf71
Struchlak, Joey (ECSA) <Joey.Struchlak@sa.gov.au>
2024-11-22 11:54:05 +1030

    Created Routes And Navagation Links

--- EmployeeMangment/src/app/app.routes.ts
+++ EmployeeMangment/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { EmployeeFormComponent } from '../components/employee-form/employee-form.component';
+import { EmployeeTableComponent } from '../components/employee-table/employee-table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: EmployeeTableComponent},
+    {path: 'create', component: EmployeeFormComponent},
+    {path: 'employees', redirectTo: '', pathMatch: 'full'}
+];
