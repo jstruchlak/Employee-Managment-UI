f3da3ab3862b37f1ef8b2e3fd18bdcda6b447fac
Struchlak, Joey (ECSA) <Joey.Struchlak@sa.gov.au>
2024-11-25 15:39:47 +1030

    Added EDIT and Delete Logic (component, routes and service)

--- EmployeeMangment/src/components/employee-table/employee-table.component.ts
+++ EmployeeMangment/src/components/employee-table/employee-table.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../../models/employee';
 import { EmployeeService } from '../../services/employee.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NumberSymbol } from '@angular/common';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -15,13 +16,28 @@ export class EmployeeTableComponent {
 
   employees: Employee[] = [];
 
-  constructor(private employeeService: EmployeeService){}
+  constructor(private employeeService: EmployeeService, private router: Router){}
 
   ngOnInit(){
     this.employeeService.getEmployees().subscribe((data: Employee[]) => {
       this.employees = data;
       console.log(data);
     });
+  }
+
+  deleteEmployee(id: number): void {
+    this.employeeService.deleteEmployee(id).subscribe({
+      next: (response) => {
+        this.employees = this.employees.filter(e => e.id !== id)
+      },
+      error: (err) => {
+        console.log('Error deleting ', err)
+      }
+    })
+  }
+
+  editEmployee(id: number): void {
+    this.router.navigate(['edit', id])
   }
 
 }
